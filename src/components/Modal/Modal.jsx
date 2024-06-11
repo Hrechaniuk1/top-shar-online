2aaa66205dc1b09718de869aafeab58439ae11da
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-06-11 12:03:59 +0300

    done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,8 @@
 import Modal from 'react-modal';
 import { useState } from 'react';
 import { nanoid } from '@reduxjs/toolkit';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup'
 
 // ------------------------------------------
 
@@ -36,6 +37,26 @@ export default function ModalOrder({isOpen, onClose, orderId,}) {
         address: '',
         time: ''
     }
+
+    const Schema = Yup.object().shape({
+        name: Yup.string()
+            .matches(/^[А-Яа-яіІЇїЄєҐґ\s]+$/, 'Ваше ім\'я українською')
+            .min(2, 'Ім\'я закоротке')
+            .max(15, 'Надто довге')
+            .required('Не забудьте вказати Ваше ім\'я'),
+        number: Yup.string()
+            .matches(/^\d{5,12}$/, 'Невірний формат: 360111111111')
+            .required('Маємо знати, як з Вами зв\'язатись'),
+        future: Yup.string()
+            .matches(/^[А-Яа-яЇїЄєҐґ\s]{0,20}$/, 'Українською, та не більше 20 символів будь ласка'),
+        time: Yup.string()
+            .matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, 'Час у форматі ГГ:ХХ')
+            .required('То о котрій доставити?'),
+        address: Yup.string()
+            .matches(/^[А-Яа-яЇїЄєҐґ0-9\s,.-]+$/, 'Невірний формат')
+            .required('То куди привезти?')
+    });
+    
     
     function onSubmit(values) {
         console.log([values, orderedSet])
@@ -60,18 +81,24 @@ export default function ModalOrder({isOpen, onClose, orderId,}) {
             <Formik
             initialValues={initial}
             onSubmit={onSubmit}
+            validationSchema={Schema}
             >
                 <Form className={css.form}>
                     <label htmlFor="orderName"> Ваше ім&apos;я</label>
                     <Field type='text' name='name' id='orderName'></Field>
+                    <ErrorMessage className={css.error} name='name' component='span'></ErrorMessage>
                     <label htmlFor="orderTel">Ваш телефон</label>
                     <Field type='tel' name='number' id='orderTel'></Field>
+                    <ErrorMessage className={css.error} name='number' component='span'></ErrorMessage>
                     <label htmlFor="orderFutures">Коментар</label>
                     <Field type='text' name='future' id='orderFutures'></Field>
+                    <ErrorMessage className={css.error} name='future' component='span'></ErrorMessage>
                     <label htmlFor="orderAddress">Ваша адреса</label>
                     <Field type='text' name='address' id='orderAddress'></Field>
+                    <ErrorMessage className={css.error} name='address' component='span'></ErrorMessage>
                     <label htmlFor="orderTime">Час доставки</label>
                     <Field type='text' name='time' id='orderTime'></Field>
+                    <ErrorMessage className={css.error} name='time' component='span'></ErrorMessage>
                     <button type='submit'>Замовити</button>                    
                 </Form>
             </Formik></div>) : (<div> 
